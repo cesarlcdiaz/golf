2d7d119a98ea3422fe1784043dd7e8dfc294d988
cesar diaz <cesarlcdiaz@gmail.com>
2016-01-05 12:49:25 -0800

    #2 User can get tips to improve game score

---
+++ js/elements.js
@@ -0,0 +1,7 @@
+var aboutHeader = document.getElementById('aboutgolf');
+var information = document.createElement('h1');
+var firstText = document.createTextNode('Golf is a club and ball sport in which players use various clubs to hit balls into a series of holes on a course in as few strokes as possible.Golf is one of the few ball games that do not require a standardized playing area. The game is played on a course with an arranged progression of either nine or 18 holes. Each hole on the course must contain a tee box to start from, and a putting green containing the actual cup. There are other standard forms of terrain in between, such as the fairway, rough, and hazards, but each hole on a course is unique in its specific layout and arrangement.Golf is played for the lowest number of strokes by an individual, known as stroke play, or the lowest score on the most individual holes in a complete round by an individual or team, known as match play. Stroke play is the most commonly seen format at all levels.');
+aboutHeader.appendChild(information);
+information.setAttribute('style', 'color : #FFFFFF');
+information.appendChild(firstText);
+
