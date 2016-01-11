750aa6f1bbdb152e9db0605565e2f5f6fe6a1d11
cesar diaz <cesarlcdiaz@gmail.com>
2016-01-11 09:48:11 -0800

    remove folder from repository

--- homepage/homepage.js
+++
@@ -1,8 +0,0 @@
-var newDiv = document.createElement('div');
-document.body.appendChild(newDiv);
-var newP = document.createElement('h1');
-newDiv.appendChild(newP);
-var newText = document.createTextNode("Easy 2 Find Golf");
-newP.appendChild(newText);
-newP.setAttribute('style', 'color : #FFFFFF');
-
--- homepage/image.js
+++
@@ -1,4 +0,0 @@
-var img = document.createElement('img');
-img.src = 'images/golfgreen.jpeg';
-img.setAttribute('style','img-responsive');
-document.getElementById('container').appendChild(img);
