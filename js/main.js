dbfe92a05557e60c055808ace5a8f324a5b3f87a
cesar diaz <cesarlcdiaz@gmail.com>
2016-01-15 09:33:23 -0800

    #1 User can find golf course on map.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,65 @@
-function initMap() {
-  var myLatLng = {lat: 33.672340, lng: -117.849041};
-
+function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: myLatLng
+    center: {lat: 39.790117, lng: -96.584307},
+    zoom: 4,
+    scrollwheel: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
   });
 
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 
   var locations = [];
 
@@ -52,9 +106,6 @@ function initMap() {
   locations.push({name: "Newport beach Golf Course", address:"3100 Irvine Ave, Newport Beach, CA 92660", url:"newportbeachgolfcoursellc.com", phone:"(949) 852-8681", latlng : new google.maps.LatLng(33.660791,-117.878921)});
 
   locations.push({name: "Desert Willow", address:"38-995 Desert Willow Dr, Palm Desert, CA 92260", url:"desertwillow.com", phone:"(760) 346-0015", latlng : new google.maps.LatLng(33.765671,-116.366058)});
-  /*
-  locations.push({name: "", address:"", web:"", phone:"", latlng : new google.maps.LatLng()});
-  */
 
   for(var i=0; i<locations.length; i++){
     var marker = new google.maps.Marker({position: locations[i].latlng, map:map, title:locations[i].name});
@@ -69,7 +120,7 @@ function initMap() {
         infoWindow.open(map, marker);
       }
     })(marker, content));
-  //google.maps.event.addDomListener(window,'load', initialize);
-  
   }
+  
 }
+
