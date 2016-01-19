2a8bcecd024e2a5d603dd414c7b15071d5bb7b12
cesar diaz <cesarlcdiaz@gmail.com>
2016-01-19 08:43:57 -0800

    #1 User can find golf course on map.

--- js/elements.js
+++ js/elements.js
@@ -1,24 +1,24 @@
 var aboutHeader = document.getElementById('aboutgolf');
-var information = document.createElement('h2');
-var firstText = document.createTextNode('Golf is a club and ball sport in which players use various clubs to hit balls into a series of holes on a course in as few strokes as possible.','Golf is a game whose aim is to hit a very small ball into an even smaller hole', 'with weapons singularly ill-designed for the purpose. On a recent survey', '80 percent of golfers admitted cheating. The other 20 percent lied.');
+var information = document.createElement('h1');
+var firstText = document.createTextNode('Find Any Golf Course On Your Area!');
 aboutHeader.appendChild(information);
-information.setAttribute('style', 'color : #040303');
+information.setAttribute('style', 'color : #FFFFFF');
 information.appendChild(firstText);
 
-var aboutHeader = document.getElementById('funnygolf');
+var aboutHeader = document.getElementById('golfweather');
 var information = document.createElement('h2');
-var firstText = document.createTextNode('Golf is a game whose aim is to hit a very small ball into an even smaller hole, with weapons singularly ill-designed for the purpose.');
+var firstText = document.createTextNode('Is it a good day to tee off?');
 aboutHeader.appendChild(information);
-information.setAttribute('style', 'color : #040303');
+information.setAttribute('style', 'color : #FFFFFF');
 information.appendChild(firstText);
-
+/*
 var aboutHeader = document.getElementById('surveygolf');
 var information = document.createElement('h2');
 var firstText = document.createTextNode('On a recent survey, 80 percent of golfers admitted cheating. The other 20 percent lied.');
 aboutHeader.appendChild(information);
 information.setAttribute('style', 'color : #040303');
 information.appendChild(firstText);
-
+*/
 
 /*var elem = document.createElement("img");
 elem.setAttribute("src", "images/logos.png");
